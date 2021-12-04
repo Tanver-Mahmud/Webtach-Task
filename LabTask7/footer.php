61e6389da2f68a4ac96898c38a03140f779bfcc4
Tanver-Mahmud <55835139+Tanver-Mahmud@users.noreply.github.com>
2021-12-04 13:01:46 +0600

    Add files via upload

---
+++ LabTask7/footer.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style-1.css">
+</head>
+<body>
+	
+	<div class="container" align="center">
+    <div id = "a">
+  <div class="row">
+    <div class="col-6">
+     <b>Address</b><br>
+	  A/633/1<br>
+	  road no-21<br>
+	Khilgaon,Dhaka
+    </div>
+   
+    <div class="col-6">
+      <b>Contact Us</b><br>
+    01798989565657
+    </div>
+   
+  </div>
+   <div class="col col-lg-12"><br>
+      <p align = "center" >[Copyright © 2021 ]</p>
+    </div>
+</div>
+
+</div>
+
+
+</body>
+</html>
---
+++ LabTask7/log-in.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<style type="text/css">
+	</style>
+</head>
+
+<body>
+	<div class="container" style="margin: 80px auto 100px; ">
+  		<div class="row" align="center" >
+    		<div class="col-12" >
+				<div class="card" style="width: 18rem; border-width: 5px;" align="center " >
+  					
+  					<div class="card-body" style="background-color: #32a852">
+    					<h4 style="color:#53f57f" class="card-title"><b>LOG IN</b></h4>
+					    <b>User Name: </b>
+						<input type="text" size= "25" name="username" placeholder="Enter your name">
+						<br><br>
+						<b>Password:</b>
+					  	<input type="password" size="25" name="password" placeholder="Enter your password">
+					  	<br><br>
+    					<a href="#" class="btn btn-primary">Log In</a><br>
+
+    					 <a href="" class="active" style="color: #53f57f;">Forgot password?</a><br>
+  						<p style="font-style: italic;">Don't have an account?<a href="p" class="active" style="color: #53f57f;">Sign Up</a></p> 
+  					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
